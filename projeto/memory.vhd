1e6c8bfd7172a891096ef9b99bac2de90fcbb161
rotta-mat <matheus.rotta@conpec.com.br>
2018-05-29 17:00:42 -0300

    adicionadas algumas entidades, preciso pensar ainda quem é a top-level

---
+++ projeto/memory.vhd
@@ -0,0 +1,60 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+-- Quartus Prime VHDL Template
+-- Single port RAM with single read/write address 
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity memory is
+
+	--generic 
+	--(
+	--	DATA_WIDTH : natural := 6;
+	--	ADDR_WIDTH : natural := 7
+	--);
+
+	port 
+	(
+		Clock : in std_logic;
+		Address : in natural range 0 to 99;
+		Data : in std_logic_vector(5 downto 0);
+		Q : out std_logic_vector(5 downto 0);
+		WrEn : in std_logic
+	);
+
+end memory;
+
+architecture direct of memory is
+
+	-- Build a 2-D array type for the RAM
+	subtype word_t is std_logic_vector(5 downto 0);
+	type memory_t is array(99 downto 0) of word_t;
+
+	-- Declare the RAM signal.	
+	signal ram : memory_t;
+
+	-- Register to hold the address 
+	signal addr_reg : natural range 0 to 99;
+	
+	signal read : std_logic_vector(5 downto 0);
+
+begin
+
+	process(Clock, Address)
+	begin
+	if(rising_edge(Clock)) then
+		if(WrEn = '1') then
+			ram(Address) <= Data;
+		end if;
+	end if;
+	
+	-- Register the address for reading
+	addr_reg <= Address;
+	end process;
+
+	Q <= ram(addr_reg);
+
+end direct;
---
+++ projeto/random.vhd
@@ -0,0 +1,32 @@
+library ieee;
+use ieee.math_real.all;
+
+entity random is
+
+port 
+	(
+		Clock : in std_logic;
+		bomb_site : out natural range 0 to 99;
+		
+		
+	);
+
+end random;
+
+architecture behavior of random is 
+
+signal rand_num : integer := 0;
+
+begin
+
+process (Clock)
+    variable seed1, seed2: positive;               -- seed values for random generator
+    variable rand: real;   -- random real-number value in range 0 to 1.0  
+    variable range_of_rand : real := 99.0;    -- the range of random values created will be 0 to +99.
+begin
+    uniform(seed1, seed2, rand);   -- generate random number
+    rand_num <= integer(rand*range_of_rand);  -- rescale to 0..99, convert integer part 
+    wait for 10 ns;
+end process;
+
+end behavior;
