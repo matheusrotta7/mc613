434c29b29589e82c9e7626ad219090d09302a922
rotta-mat <matheus.rotta@conpec.com.br>
2018-06-09 16:17:43 -0300

    update

--- projeto/count_mines.vhd
+++ projeto/count_mines.vhd
@@ -15,22 +15,26 @@ end count_mines;
 -- Use Clause(s) (optional)
 
 signal num_bombs : std_logic_vector (3 downto 0);
-signal 
+signal aux : std_logic_vector (6 downto 0);
+signal is_bomb : std_logic;
+
 
 architecture behavior of count_mines is
 
 begin
 
+aux <= read_data;
+is_bomb <= read_data(5);
 
 
 process (clock, clock64)
 begin
 	if (clock'event and clock = '1') then
-		
-				
+		if (clock64 = '0') then
+			num_bombs =< num_bombs + is_bomb;
+		else
+			aux (3 downto 0) <= num_bombs;
+			new_data <= aux;
+		end if;
 	end if;
-
-
-	
-
 end behavior;
